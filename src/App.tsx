c1249914a2d5f5ae19483a0be00b418b0e2ce166
Miguel Rivas <mrivaslezama@gmail.com>
2023-11-07 22:14:27 -0300

    actualizando

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  return <main className="bg-gray-50">
+    <div className="container mx-auto p-6">
 
-  return (
-    <>
+      <div className="w-full flex items-center justify-between">
+        <a className="flex items-center text-gray-900 
+                    no-underline hover:no-underline font-bold 
+                    text-2xl lg:text-2xl" href="#">
+          Freelance - Web developer
+        </a>
+
+        <div className="flex w-1/2 justify-end">
+          <div className="">
+            <button type="button" className="inline-block 
+                  px-6 py-2.5 bg-blue-600 text-white 
+                  font-medium text-xs leading-tight 
+                  uppercase rounded shadow-md hover:bg-rose-700 
+                  hover:shadow-lg focus:bg-rose-700 
+                  focus:shadow-lg focus:outline-none 
+                  focus:ring-0 active:bg-rose-800 active:shadow-lg 
+                  transition duration-150 ease-in-out">Contact</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  <div className="container mx-auto p-6">
+    <div className="w-full flex items-center justify-between">
+      <h1 className="flex items-center text-gray-900 
+                    no-underline hover:no-underline font-bold 
+                    text-2xl lg:text-4xl">
+         Hi, I am Miguel
+      </h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        <div className="mt-12 md:mt-0">
+          <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase"> Creamos productos digitales</h3>
+          <span></span>
+          <h3> Asesoramos todas las etapas del desarrollo web</h3>
+        </div>
+      </div>  
+    </div>
+  </div>
+
+  <div>
+    
+
+  </div>
+
+  </main>
 }
 
 export default App
